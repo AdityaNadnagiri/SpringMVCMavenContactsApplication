25f611d4a8da10627cbd067f40f2c6217eed0b63
nanda <nanda@192.168.1.108>
2017-12-05 16:56:10 -0500

    url Refactored

--- src/main/java/com/aditya/controller/TestController.java
+++ src/main/java/com/aditya/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 	
-	@RequestMapping("/test/helloWorld")
+	@RequestMapping("/test/hello")
 	public String helloWorld() {
 		return "hello";
 		

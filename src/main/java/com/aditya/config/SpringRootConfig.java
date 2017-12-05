4dd1c59dcea93e832d024777919262a448ed53e4
nanda <nanda@192.168.1.108>
2017-12-05 17:22:22 -0500

    dbcp

--- src/main/java/com/aditya/config/SpringRootConfig.java
+++ src/main/java/com/aditya/config/SpringRootConfig.java
@@ -1,10 +1,25 @@
 package com.aditya.config;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages= {"com.aditya"})
 public class SpringRootConfig {
+	
+	@Bean
+	public BasicDataSource basicDataSource() {
+		
+		BasicDataSource ds=new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/capp_db");
+		ds.setUsername("root");
+		ds.setPassword("");
+		
+		return ds;
+		
+	} 
 
 }

3870716afcc9cba177d3e80fb85ec60bd3e9c2c2
nanda <nanda@Aditya-Nandagiri>
2017-12-26 19:48:33 -0500

    errors and warnings cleared

--- src/main/java/com/aditya/config/SpringWebConfig.java
+++ src/main/java/com/aditya/config/SpringWebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @ComponentScan(basePackages= {"com.aditya"})
 @EnableWebMvc
